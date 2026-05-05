ff33662b6a27540d69633624c91854ac1b63ce78
Aryan Shah <qgm3818@aut.ac.nz>
2026-05-05 15:35:36 +1200

    Add landing page

--- job-application-tracker/src/main.tsx
+++ job-application-tracker/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App, { ProtectedRoute } from './App.tsx'
+import { ProtectedRoute } from './App.tsx'
 import Home from './pages/Home.tsx'
 import { AuthProvider } from './contexts/AuthProvider.tsx'
 import { NotificationsProvider } from './contexts/NotificationsContext.tsx'
